3720be03a5d25b3d234da52a5ffb7bdaaa5f6ffd
saida brito <saidabrito57@gmail.com>
2021-04-05 20:16:01 -0500

    Agregada request a la api para obtener data de categorías

---
+++ Client/index.js
@@ -0,0 +1,35 @@
+import { createCategory } from "./modules/createCategory.js";
+import { productsWrapper, createProduct } from "./modules/createProduct.js";
+
+const apiRoot = "http://localhost:3000";
+
+function run() {
+  getCategories();
+  
+}
+
+
+async function getData(path) {
+  let response = await fetch(`${apiRoot}${path}`);
+  let data = await response.json();
+  if (response.ok) {
+    return data;
+  } else {
+    throw new Error("an error occurred while processing your request");
+  }
+}
+
+function getCategories() {
+  getData("/categories")
+    .then((data) =>
+      data.forEach((category) => {
+        createCategory(category.name).addEventListener("click", () => {
+          productsWrapper.textContent = "";
+          getProducts(`/products?category=${category.id}`, true);
+        });
+      })
+    )
+    .catch((e) => alert(`${e}`));
+}
+
+run();
