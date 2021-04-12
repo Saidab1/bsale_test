d717b23e1ded8765e3051ca67d20e17678c340cb
saida brito <saidabrito57@gmail.com>
2021-04-12 17:37:45 -0500

    Agregados comentarios en index.js

--- Client/index.js
+++ Client/index.js
@@ -6,20 +6,33 @@ const apiRoot = "https://bsale-test-api.herokuapp.com";
 
 const searchForm = document.querySelector(".search_form");
 const logo = document.querySelector(".store_logo");
-let searchValue;
 const observerDiv = document.querySelector("#observer");
+let searchValue;
+
+//tracks loading state
 let loading = false;
+
+//indicates results page
 let productsPage = 1;
+
+//tracks if a category is select
 let categorySelected = false;
 let categoryId;
+
+//tracks if a search is made
 let searchSelected = false;
 
-
+/**
+ * start the application
+ */
 function run() {
-  getCategories();
+  createCategories();
   getProducts("/products", true);
 }
 
+/**
+ * Utility to query API and parse the json response
+ */
 async function getData(path) {
   let response = await fetch(`${apiRoot}${path}`);
   let data = await response.json();
@@ -30,7 +43,10 @@ async function getData(path) {
   }
 }
 
-function getCategories() {
+/**
+ * creates category button and add event listener to each one
+ */
+function createCategories() {
   getData("/categories")
     .then((data) =>
       data.forEach((category) => {
@@ -47,6 +63,10 @@ function getCategories() {
     .catch((e) => alert(e));
 }
 
+/**
+ * if get data from api creates products,
+ * otherwise if is a search indicates no result found;
+ */
 function getProducts(path, displayLoading) {
   loading = true;
   if (displayLoading) showLoading();
@@ -69,6 +89,10 @@ function getProducts(path, displayLoading) {
     .catch((e) => alert(e));
 }
 
+/**
+ * creates loading message if is loading and loading message doesn't exist,
+ * otherwise removes it from DOM
+ */
 function showLoading() {
   const loadingMessage = document.getElementById("loading_message");
 
@@ -86,6 +110,9 @@ function showLoading() {
   }
 }
 
+/**
+ * add event listener to BsaleShop(logo) to get all products
+ */
 logo.addEventListener("click", () => {
   categorySelected = false;
   searchSelected = false;
@@ -94,6 +121,10 @@ logo.addEventListener("click", () => {
   getProducts("/products", true);
 });
 
+/**
+ * add event listener to get user search and gets
+ * products that includes it
+ */
 searchForm.addEventListener("submit", (Event) => {
   categorySelected = false;
   searchSelected = true;
@@ -106,6 +137,10 @@ searchForm.addEventListener("submit", (Event) => {
   Event.target.firstElementChild.value = "";
 });
 
+/**
+ * allows to load more products when a category is selected or not,
+ *  or in a search
+ */
 const observer = new IntersectionObserver((entries) => {
   const firstentry = entries[0];
 
